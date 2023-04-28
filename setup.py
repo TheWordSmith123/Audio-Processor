671a2035243c38d1257633c039dc9300d0209199
WordSmith123 <drewfhorvath@gmail.com>
2023-04-27 23:50:59 -0400

    Added ReadMe to PyPi

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="audioprocessor",
@@ -15,6 +19,8 @@ setup(
     author="Andrew Horvath",
     author_email="drewfhorvath@gmail.com",
     description="A Python package for recording, transcribing, and converting audio",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url="https://github.com/TheWordSmith123/Audio-Processor",
     classifiers=[
         "Development Status :: 3 - Alpha",
